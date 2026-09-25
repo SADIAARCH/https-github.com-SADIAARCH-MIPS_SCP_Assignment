dee601cd6f0e578b73d3546f27c04c28728cc60c
Sadia Rahman <sr1247189@gmail.com>
2026-09-25 18:58:33 +0600

    Upload MIPS Single-Cycle Processor design files and Assembler

---
+++ MIPS_SCP_Assignment/MIPS_SCP_tb.v
@@ -0,0 +1,34 @@
+// file: MIPS_SCP_tb.v
+// Testbench for MIPS_SCP
+
+`timescale 1ns/1ns
+
+module MIPS_SCP_tb;
+
+	//Inputs
+	reg clk;
+    reg reset;
+
+	//Outputs
+
+
+	//Instantiation of Unit Under Test
+	MIPS_SCP uut (
+		.clk(clk),
+		.reset(reset)
+	);
+
+    always
+        #50 clk=!clk;
+	initial begin
+    	clk=0;
+	reset=1;
+	#1000; //cycle 1
+        repeat(10) begin
+	reset=0;
+	#100;
+        end
+        #10;
+  	end
+endmodule
+
